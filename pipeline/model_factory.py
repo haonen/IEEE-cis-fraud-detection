501e03a66ceb2b33e62cbbc9601b5621b218d199
Mengxiao Lin <linmx0130@gmail.com>
2019-08-21 08:53:45 +0800

    new adaboost with adjustable base estimator

--- pipeline/model_factory.py
+++ pipeline/model_factory.py
@@ -11,6 +11,7 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+import sklearn.ensemble
 
 import yaml
 from collections import OrderedDict
@@ -43,6 +44,18 @@ def get_models(config):
     #pdb.set_trace()
     for name, params in config.items():
         constructor = globals()[name]
+        if name in dir(sklearn.ensemble):
+            if 'base_estimator' in params:
+                if type(params['base_estimator']) is list:
+                    # this parameter has been modified
+                    continue
+                base_estimator_config = params['base_estimator']
+                base_estimator = list(get_models(base_estimator_config))
+                assert len(base_estimator) == 1, "Too much estimators are generated for AdaBoost"
+                print("Base estimator for Adaboost")
+                print(base_estimator[0][1])
+                params['base_estimator'] = [base_estimator[0][1]]
+
         if name == 'GaussianNB':
             models =  [constructor()]
         else:
